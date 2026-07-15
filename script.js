df5c1149c9bb09fabfcda33363e19d0ca5cf9e29
preshu1 <shresthapreshu26@gmail.com>
2026-07-15 20:32:56 +0545

    implemented findBookByTitle function

--- script.js
+++ script.js
@@ -28,9 +28,20 @@ const library = {
   getUnreadBooks() {
     return this.getTotalBooks() - this.getReadBooks();
   },
+
+  findBooksByTitle(title) {
+    for (const book of this.books) {
+      if (book.title.toLowerCase() === title.toLowerCase()) {
+        return book;
+      }
+    }
+    return null;
+  },
 };
 library.addBook("twilight", "Stephenie Meyer", 498, true);
 console.log(library.books);
 console.log("Total Number of Books:", library.getTotalBooks());
 console.log("Number of Read Books:", library.getReadBooks());
 console.log("Number of Unread Books:", library.getUnreadBooks());
+const booksFound = library.findBooksByTitle("TwilIght");
+console.log("Book Found", booksFound);
