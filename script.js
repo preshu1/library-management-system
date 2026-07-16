ee448edb37aae274778353803008bb5d385abbda
preshu1 <shresthapreshu26@gmail.com>
2026-07-16 15:02:19 +0545

    implemented markasread() and displayallbooks() functionality

--- script.js
+++ script.js
@@ -37,11 +37,37 @@ const library = {
     }
     return null;
   },
+
+  markAsRead(title) {
+    const book = this.findBooksByTitle(title);
+    if (book) {
+      book.hasRead = true;
+      return true;
+    }
+    return false;
+  },
+
+  displayAllBooks() {
+    this.books.forEach((book, index) => {
+      console.log(`${index + 1}. ${book.title} by ${book.author}`);
+      console.log(
+        `Pages: ${book.pages} | Read: ${book.hasRead ? "Yes" : "No"}`,
+      );
+    });
+  },
 };
 library.addBook("twilight", "Stephenie Meyer", 498, true);
+library.addBook("1984", "George Orwell", 328, false);
+library.addBook("The Hobbit", "J.R.R. Tolkien", 310, false);
+library.addBook("Harry Potter", "J.K. Rowling", 309, true);
 console.log(library.books);
 console.log("Total Number of Books:", library.getTotalBooks());
 console.log("Number of Read Books:", library.getReadBooks());
 console.log("Number of Unread Books:", library.getUnreadBooks());
 const booksFound = library.findBooksByTitle("TwilIght");
 console.log("Book Found", booksFound);
+library.markAsRead("1984");
+console.log("After marking 1984 as marked: ");
+console.log("Read Books: ", library.getReadBooks());
+console.log("Unread Books: ", library.getUnreadBooks());
+library.displayAllBooks();
